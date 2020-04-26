3f396f857ecc64bcce598bafb532303ac854a33b
unknown <mouhatall59@gmail.com>
2020-04-26 00:53:19 +0000

    fist version

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('#open_modal').click(function(){
+        $('#modal_open').css(
+            {
+                'display':'block'
+            }
+        );
+    });
+});
